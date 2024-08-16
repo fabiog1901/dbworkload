517df943cb6c214259fb1f52033f670302a258d3
Fabio Ghirardello <fabio@cockroachlabs.com>
2024-08-16 14:38:56 -0400

    adding dockerfile

---
+++ docker/build.sh
@@ -0,0 +1,15 @@
+
+VERSION=`egrep "^version = " ../pyproject.toml | cut -d "=" -f2 | tr -d '[:space:]' | tr -d '"'`
+
+docker build -t fabiog1901/dbworkload:$VERSION .
+
+docker push fabiog1901/dbworkload:$VERSION
+
+docker build -t fabiog1901/dbworkload .
+
+docker push fabiog1901/dbworkload
+
+#docker image rm fabiog1901/dbworkload:v0.2.2
+#docker image rm fabiog1901/dbworkload
+
+#docker run --rm fabiog1901/dbworkload --version
